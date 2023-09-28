658449419c40c3f6999dd1064f023cd25ecf2cdd
antohella <140485161+Trouh@users.noreply.github.com>
2023-09-28 18:41:36 +0300

    Update Game_commands.py

--- Game_commands.py
+++ Game_commands.py
@@ -1,7 +1,7 @@
-# Name: Команды для игровых ботов Bfg, Bfgb, MineEVO.
+# name: Команды для игровых ботов
 # meta developer: @MetaModules
 # Commands:
-# .bfg  | .evo  | .bfgb
+# .bfg  | .evo  | .bfgb  | .iris
 
 import time
 
@@ -138,6 +138,52 @@ class BfgMod(loader.Module):
                     response = await response
                 except YouBlockedUserError:
                     await message.edit("<b>Разблокируй @bfgbunker_bot!</b>")
+                    return
+        if response.text:
+            await message.client.send_message(
+                message.to_id, f"<b>{response.text}</b>"
+            )
+            await message.delete()
+        if response.media:
+            await message.client.send_file(
+                message.to_id, response.media, reply_to=reply.id if reply else None
+            )
+            await message.delete()
+            
+            
+    async def iriscmd(self, message):
+        """— Выполнить команду в боте Iris в любом чате"""
+        chat = "@iris_black_bot"
+        args = utils.get_args_raw(message)
+        text = f"{args}"
+        reply = await message.get_reply_message()
+        if not text and not reply:
+            await message.edit("<b>Нет текста или реплая!</b>")
+            return
+        await message.edit("<b>Отправляю...</b>")
+        async with message.client.conversation(chat) as conv:
+            if text:
+                try:
+                    response = conv.wait_event(
+                        events.NewMessage(incoming=True)
+                    )
+                    await message.client.send_message(chat, text)
+                    response = await response
+                except YouBlockedUserError:
+                    await message.edit("<b>Разблокируй @iris_black_bot!</b>")
+                    return
+            else:
+                try:
+                    user = await utils.get_user(reply)
+                    response = conv.wait_event(
+                        events.NewMessage(incoming=True)
+                    )
+                    await message.client.send_message(
+                        chat, f"{reply.raw_text} (с) {user.first_name}"
+                    )
+                    response = await response
+                except YouBlockedUserError:
+                    await message.edit("<b>Разблокируй @iris_black_bot!</b>")
                     return
         if response.text:
             await message.client.send_message(
